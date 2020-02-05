c4963fcc767e42b3e10521e6635d1aab6c387f45
Jordan Verasamy <j.verasamy@gmail.com>
2020-02-05 16:55:49 -0500

    add defeated by

--- app/commands/record_command.py
+++ app/commands/record_command.py
@@ -46,9 +46,9 @@ class RecordCommand:
         logger.info(f"[emoji_name={emoji_name}]")
         try:
             emoji = Emoji.objects.get(name=emoji_name)
-            full_record = f' Defeated: {" ".join(emoji.defeated())}' if detail_level == 'full' else ''
+            full_record = f' Defeated: {" ".join(emoji.defeated())} Was defeated by: {" ".join(emoji.defeated_by())}' if detail_level == 'full' else ''
             wins, losses, ties = emoji.record()
             client.post_message(event, f"{number_map.get(wins, wins)} {number_map.get(losses, losses)}"
                                        f" {number_map.get(ties, ties)} {full_record}")
         except ObjectDoesNotExist:
-            client.post_message(event, 'Could not found that emoji sry')
+            client.post_message(event, 'Could not find that emoji, sorry.')
